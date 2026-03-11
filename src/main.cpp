0b35f216784af5e2f4566b01ed118aa272a79851
Maxim Kozlov <maximko.ca@gmail.com>
2026-03-10 20:00:01 -0400

    Glad and fix shit

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,12 @@
 #include <iostream>
-#include <raylib.h>
+#include <glad/glad.h>
+#include <test/test.h>
 
 int main() {
 
-	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-	InitWindow(800, 450, "window name");
+	std::cout << "Init\n";
 
-	Color c;
-
-	c.r = 255;
-	c.g = 0;
-	c.b = 0;
-	c.a = 255;
-
-
-	while (!WindowShouldClose())
-	{
-		BeginDrawing();
-		ClearBackground(RAYWHITE);
-
-		
-
-		DrawText("Congrats!", 190, 200, 54, c);
-
-		EndDrawing();
-	}
-
-	CloseWindow();
+	Test();
 
 	return 0;
 }
---
+++ src/test/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <test/test.h>
+
+void Test() {
+	std::cout << "Testing!" << std::endl;
+}
+
